84d2c088fc157b0d2060fc780104ca744a957808
infow <infow@northseaway.com>
2017-06-24 15:26:43 +0900

    アプリケーションにウェブブラウザ機能を組み込むチュートリアル https://www.youtube.com/watch?v=uYcbtGdFZqc 用プロジェクト

---
+++ Sample/WebBrowserTest/WebBrowserUnit.pas
@@ -0,0 +1,49 @@
+﻿unit WebBrowserUnit;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Edit, FMX.WebBrowser;
+
+type
+  TForm1 = class(TForm)
+    ToolBar1: TToolBar;
+    Button1: TButton;
+    Edit1: TEdit;
+    WebBrowser1: TWebBrowser;
+    procedure Edit1KeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
+      Shift: TShiftState);
+    procedure Button1Click(Sender: TObject);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if WebBrowser1.CanGoBack then
+  begin
+    WebBrowser1.GoBack;
+  end;
+end;
+
+procedure TForm1.Edit1KeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
+  Shift: TShiftState);
+begin
+  if key = vkReturn then
+  begin
+    WebBrowser1.Navigate(Edit1.Text);
+  end;
+end;
+
+end.
